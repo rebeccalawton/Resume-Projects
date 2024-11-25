dfa77e140d8b9275a2d1398347ad66791a8de845
rebeccalawton <rebeccalawton2027@u.northwestern.edu>
2024-11-24 23:11:46 -0600

    Update pipelinedMAC.v

--- pipelinedMAC.v
+++ pipelinedMAC.v
@@ -1,5 +1,12 @@
 //Rebecca Lawton
 
+/* At every clock cycle, MAC performs IN*WEIGHT and accumulates with
+its internal stored value from previous cycle to generate the OUT value.
+The accumulator starts from 0 value and perform accumulations for 9 cycles. After
+that, B starts from 0 again, A takes the new input from Multiplier, and OUT gives the
+final MAC output.
+*/
+
 `timescale 1ns/10ps
   module MAC(IN, W, clk, rstb, OUT);
    input signed [3:0] IN, W;
